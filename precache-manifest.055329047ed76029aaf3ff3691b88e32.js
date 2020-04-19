8b83669f84060b5deefefa9f2bfcfae38b6404bc
Usama Awan <underusama@gmail.com>
2020-04-19 22:52:36 +0500

    Updates

---
+++ precache-manifest.055329047ed76029aaf3ff3691b88e32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cddfbb1b80446a2f683f9a2f88fae46",
+    "url": "/React-Calculator/index.html"
+  },
+  {
+    "revision": "2363e112503647632cf9",
+    "url": "/React-Calculator/static/css/main.2c25c9d4.chunk.css"
+  },
+  {
+    "revision": "aa04b33db929b2d33c5a",
+    "url": "/React-Calculator/static/js/2.7e4d6f6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Calculator/static/js/2.7e4d6f6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2363e112503647632cf9",
+    "url": "/React-Calculator/static/js/main.a0c4f386.chunk.js"
+  },
+  {
+    "revision": "ce946a65e3eadc49357d",
+    "url": "/React-Calculator/static/js/runtime-main.606fe7f2.js"
+  }
+]);
